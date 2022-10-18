7058eeadcc5fb3b1a53b63621cde77ab37d2b403
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2022-10-18 08:43:46 +0300

    18 lesson

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from "./cmponents/Header/Header";
 import NavBar from "./cmponents/NavBar/NavBar";
 import Profile from "./cmponents/Profile/Profile";
+import Dialogs from "./cmponents/Dialogs/Dialogs";
 
 const App = () => {
   return (
       <div className="app-wrapper">
           <Header />
           <NavBar />
-          <Profile />
+          <div className="app-wrapper-content">
+              {/*<Profile />*/}
+              <Dialogs />
+          </div>
       </div>
   );
 };
--- src/cmponents/Profile/Profile.jsx
+++ src/cmponents/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://thumbs.dreamstime.com/z/technology-banner-background-old-new-using-computer-circuits-old-machine-cogs-37036025.jpg" alt="banner"/>
             </div>
