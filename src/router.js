ceee59c2ae5f8692aa41a2462e9d618cc2ea0cce
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2022-11-17 09:55:17 +0200

    lesson 28

--- src/router.js
+++ src/router.js
@@ -2,60 +2,61 @@ import {createBrowserRouter} from "react-router-dom";
 import Profile from "./cmponents/Profile/Profile";
 import Dialogs from "./cmponents/Dialogs/Dialogs";
 import App from './App';
+const data = {
+    posts :[
+        {
+            id: 1,
+            message: 'Hi, How are you?',
+            likesCount: 12,
+        },
+        {
+            id: 2,
+            message: 'It\'s my first post',
+            likesCount: 22,
+        },
+    ],
+    dialogs : [
+        {
+            id: 1,
+            name: 'Dimych',
+        },
+        {
+            id: 2,
+            name: 'Andrey',
+        },
+        {
+            id: 3,
+            name: 'Sasha',
+        },
+        {
+            id: 4,
+            name: 'Sveta',
+        },
+        {
+            id: 5,
+            name: 'Vladyslav',
+        },
+        {
+            id: 6,
+            name: 'Yurii',
+        },
 
-let posts = [
-    {
-        id: 1,
-        message: 'Hi, How are you?',
-        likesCount: 12,
-    },
-    {
-        id: 2,
-        message: 'It\'s my first post',
-        likesCount: 22,
-    },
-]
-let dialogs = [
-    {
-        id: 1,
-        name: 'Dimych',
-    },
-    {
-        id: 2,
-        name: 'Andrey',
-    },
-    {
-        id: 3,
-        name: 'Sasha',
-    },
-    {
-        id: 4,
-        name: 'Sveta',
-    },
-    {
-        id: 5,
-        name: 'Vladyslav',
-    },
-    {
-        id: 6,
-        name: 'Yurii',
-    },
-
-];
-let messages = [
-    {
-        id: 1,
-        message: 'Hi',
-    },
-    {
-        id: 2,
-        message: 'How are you?',
-    },
-    {
-        id: 3,
-        message: 'yo?',
-    },
-];
+    ],
+    messages : [
+        {
+            id: 1,
+            message: 'Hi',
+        },
+        {
+            id: 2,
+            message: 'How are you?',
+        },
+        {
+            id: 3,
+            message: 'yo?',
+        },
+    ]
+}
 
 const router = createBrowserRouter([
     {
@@ -64,11 +65,11 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Profile posts={posts} />,
+                element: <Profile posts={data.posts} />,
             },
             {
                 path: "dialogs",
-                element: <Dialogs messages={messages} dialogs={dialogs}/>,
+                element: <Dialogs messages={data.messages} dialogs={data.dialogs}/>,
             },
             {
                 path: "news",
