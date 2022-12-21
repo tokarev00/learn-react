e1125629658b386b192efba8e681f880c52c7e67
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2022-12-21 09:47:47 +0200

    lesson 51

--- src/cmponents/Users/Users.jsx
+++ src/cmponents/Users/Users.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import styles from './users.module.css';
-
+import axios from "axios";
+import userPhoto from '../../assets/images/user-picture.png'
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
-                followed: false,
-                fullName: 'Testname',
-                status: 'test status',
-                location: {city: 'Poltava', country: "Ukraine"}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
-                followed: false,
-                fullName: 'Testname2',
-                status: 'test status 2',
-                location: {city: 'Poltava', country: "Ukraine"}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
-                followed: true,
-                fullName: 'Testname3',
-                status: 'test status 3',
-                location: {city: 'Poltava', country: "Ukraine"}
-            },
-        ]);
+        axios({method: 'GET', url:' https://social-network.samuraijs.com/api/1.0/users'}).then(response => {
+            props.setUsers(response.data.items);
+        })
     }
 
     return <div>
@@ -36,7 +14,7 @@ let Users = (props) => {
             props.users.map(u => <div className={styles.user} key={u.id}>
                 <div>
                     <div>
-                        <img src={u.photoUrl} alt="avatar" className={styles.userPhoto}/>
+                        <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="avatar" className={styles.userPhoto}/>
                     </div>
                     <div>
                         {
@@ -48,11 +26,11 @@ let Users = (props) => {
                 </div>
                 <div>
                     <span>
-                        <div>{u.fullName}</div><div>{u.status}</div>
+                        <div>{u.name}</div><div>{u.status}</div>
                     </span>
                 </div>
-                <span>{u.location.country}</span>
-                <span>{u.location.city}</span>
+                <span>{'u.location.country'}</span>
+                <span>{'u.location.city'}</span>
             </div>)
         }
     </div>
