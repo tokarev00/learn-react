5ef3654b0acc52d622b5a84ac83b0b06e15ed3d3
Vlad Tokarev <vlad.t@thewhitelabelagency.com>
2023-01-23 14:15:30 +0200

    lesson 62

--- src/cmponents/Users/Users.jsx
+++ src/cmponents/Users/Users.jsx
@@ -2,56 +2,73 @@ import React from 'react';
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user-picture.png";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 
 let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
-    for (let i = 1; i <= pagesCount; i++){
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
     return (
         <div>
-        <div>
+            <div>
+                {
+                    pages.map(p => {
+                        return <span className={props.currentPage === p && styles.selectedPage} onClick={() => {
+                            props.onPageChanged(p);
+                        }}>{p}</span>
+                    })
+                }
+            </div>
             {
-                pages.map(p => {
-                    return <span className={props.currentPage === p && styles.selectedPage} onClick={() => {
-                        props.onPageChanged(p);
-                    }}>{p}</span>
-                })
-            }
-        </div>
-        {
-            props.users.map(u => <div className={styles.user} key={u.id}>
-                <div>
+                props.users.map(u => <div className={styles.user} key={u.id}>
                     <div>
-                        <NavLink to={'/profile/' + u.id}>
-                            <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="avatar"
-                                 className={styles.userPhoto}/>
-                        </NavLink>
-                    </div>
-                    <div>
-                        {
-                            u.followed ?
+                        <div>
+                            <NavLink to={'/profile/' + u.id}>
+                                <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="avatar"
+                                     className={styles.userPhoto}/>
+                            </NavLink>
+                        </div>
+                        <div>
+                            {u.followed ?
                                 <button onClick={() => {
+                                    axios({
+                                        method: 'DELETE',
+                                        url: ' https://social-network.samuraijs.com/api/1.0/follow/' + u.id,
+                                        withCredentials: true
+                                    }).then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(u.id);
+                                        }
+                                    });
                                     props.unfollow(u.id)
-                                }}>Follow</button> :
-                                <button onClick={() => {
-                                    props.follow(u.id)
                                 }}>Unfollow</button>
-                        }
+                                :
+                                <button onClick={() => {
+                                    axios({
+                                        method: 'POST',
+                                        url: ' https://social-network.samuraijs.com/api/1.0/follow/' + u.id,
+                                        withCredentials: true
+                                    }).then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.follow(u.id);
+                                        }
+                                    });
+                                }}>Follow</button>}
+                        </div>
                     </div>
-                </div>
-                <div>
+                    <div>
                             <span>
                                 <div>{u.name}</div><div>{u.status}</div>
                             </span>
-                </div>
-                <span>{'u.location.country'}</span>
-                <span>{'u.location.city'}</span>
-            </div>)
-        }
-    </div>)
+                    </div>
+                    <span>{'u.location.country'}</span>
+                    <span>{'u.location.city'}</span>
+                </div>)
+            }
+        </div>)
 }
 
 export default Users;
--- src/cmponents/Users/UsersContainer.jsx
+++ src/cmponents/Users/UsersContainer.jsx
@@ -8,7 +8,7 @@ import Preloader from "../common/Preloader/Preloader";
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios({method: 'GET', url:` https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`}).then(response => {
+        axios({method: 'GET', url:` https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, withCredentials: true}).then(response => {
             this.props.toggleIsFetching(false);
             this.props.setUsers(response.data.items);
             this.props.setUsersTotalCount(response.data.totalCount);
@@ -17,7 +17,7 @@ class UsersContainer extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.toggleIsFetching(true);
         this.props.setCurrentPage(pageNumber);
-        axios({method: 'GET', url:` https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`}).then(response => {
+        axios({method: 'GET', url:` https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, withCredentials: true}).then(response => {
             this.props.toggleIsFetching(false);
             this.props.setUsers(response.data.items);
         });
