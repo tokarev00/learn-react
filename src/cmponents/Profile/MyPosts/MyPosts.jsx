5762d855f2b8fb1ec3d61304d101816dea12cb13
Vlad Tokarev <vlad.t@thewhitelabelagency.com>
2023-04-17 17:53:51 +0300

    lesson 87

--- src/cmponents/Profile/MyPosts/MyPosts.jsx
+++ src/cmponents/Profile/MyPosts/MyPosts.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import Post from "./Post/Post";
 import s from "./MyPosts.module.css";
-import { Field, reduxForm } from 'redux-form';
+import {Field, reduxForm} from 'redux-form';
 import {maxLength, requiredField} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormControls";
 
-const MyPosts = (props) => {
+const MyPosts =  React.memo(props => {
 
-    let postsElements = props.posts.map(post => <Post message={post.message} key={post.id} id={post.id} likesCount={post.likesCount} />)
+    let postsElements = props.posts.map(post => <Post message={post.message} key={post.id} id={post.id}
+                                                      likesCount={post.likesCount}/>)
 
     const onAddPost = (values) => {
         props.addPost(values.newPostText);
     }
 
-    return(
+    return (
         <div>
             <h3>My posts</h3>
 
-            <AddNewPostForm onSubmit={onAddPost} />
+            <AddNewPostForm onSubmit={onAddPost}/>
 
             <div className={s.posts}>
                 {postsElements}
             </div>
         </div>
     )
-}
+});
+
 const maxLength10 = maxLength(10);
 
 
