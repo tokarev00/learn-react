0582e7c61933e5ea2165e19686a2fbd1e81b08a6
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2023-04-20 22:55:55 +0300

    Updates

---
+++ precache-manifest.0354782e790def6e08b0d00f86b80149.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3937a2e65324a5afc829",
+    "url": "/learn-react/static/css/main.854913b4.chunk.css"
+  },
+  {
+    "revision": "3937a2e65324a5afc829",
+    "url": "/learn-react/static/js/main.3937a2e6.chunk.js"
+  },
+  {
+    "revision": "d5c04031624b9f8b6523",
+    "url": "/learn-react/static/js/1.d5c04031.chunk.js"
+  },
+  {
+    "revision": "e5157fb99fde89702fe3",
+    "url": "/learn-react/static/css/2.9079a057.chunk.css"
+  },
+  {
+    "revision": "e5157fb99fde89702fe3",
+    "url": "/learn-react/static/js/2.e5157fb9.chunk.js"
+  },
+  {
+    "revision": "99b22291c49e52e2951e",
+    "url": "/learn-react/static/js/3.99b22291.chunk.js"
+  },
+  {
+    "revision": "032804ba64ce528d8402",
+    "url": "/learn-react/static/js/runtime~main.032804ba.js"
+  },
+  {
+    "revision": "547a26330a54454a433d5c049c4ccb06",
+    "url": "/learn-react/index.html"
+  }
+];
